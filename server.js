fc60f0a3cf9dbdf9d02cfb7fb99bf6387a802c15
Lila Ellis <lilaellis@lilas-mpb.lan>
2023-07-09 14:47:54 -0500

    added bulma to main

--- server.js
+++ server.js
@@ -1,7 +1,1 @@
-app.use(routes);
-// router.get('/', async(req, res) => {
-//     res.render('main');
-// });
-// sequelize.sync({ force: false }).then(() => {
-app.listen(PORT, () => console.log("Now listening"));
-// });
+
