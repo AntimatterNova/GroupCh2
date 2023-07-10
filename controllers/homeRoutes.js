ecbb7d31656a7af7d1d0d134922daead4b9292ae
AntimatterNova <47792025+AntimatterNova@users.noreply.github.com>
2023-07-10 18:36:29 -0500

    Got the homepage running throught the server

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,26 +1,5 @@
 const router = require('express').Router();
 const { User } = require('../models/user');
-// const withAuth = require('../utils/auth');
-
-// Prevent non logged in users from viewing the homepage
-// router.get('/', withAuth, async (req, res) => {
-//   try {
-//     const userData = await User.findAll({
-//       attributes: { exclude: ['password'] },
-//       order: [['name', 'ASC']],
-//     });
-
-//     const users = userData.map((project) => project.get({ plain: true }));
-
-//     res.render('homepage', {
-//       users,
-//       // Pass the logged in flag to the template
-//       logged_in: req.session.logged_in,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 router.get('/', (req, res) => {
     res.render('main');
@@ -28,6 +7,10 @@ router.get('/', (req, res) => {
 
 router.get('/login', (req, res) => {
   res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
 });
 
 module.exports = router;
